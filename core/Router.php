3a2bb9c910d6951f9e45ef9d82dbc81546643086
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-02-15 11:53:24 +0200

    array $routes + fn get + fn resolve

--- core/Router.php
+++ core/Router.php
@@ -12,9 +12,44 @@ namespace app\core;
  */
 class Router
 {
+    /**
+     * This will hold all routes.
+     *
+     * routes [
+     * ['get'  => [
+     *  ['/' => function return,],
+     *  ['/about' => function return,],
+     * ],
+     * ['post' => [
+     *  ['/' => function return,],
+     *  ['/about' => function return,],
+     * ]]
+     * ]
+     *
+     *
+     * ];
+     *
+     * @var array
+     */
+    protected array $routes = [];
+
     public function __construct()
     {
-        echo "This is Router constructor";
+        echo "This is Router constructor<br>";
     }
+
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        echo "<pre>";
+        var_dump($_SERVER);
+        echo "</pre>";
+        exit;
+    }
+
 
 }
