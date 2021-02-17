501f85ec6f393227f66794bd3534c725e9be30b2
Saras Maras <sarunas.mailas@gmail.com>
2021-02-17 13:33:14 +0200

    vendor included

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '16da1a62fc2f500c5267b7cedf2c03fbbff1d398',
+    'name' => 'markuz/almost-lara',
+  ),
+  'versions' => 
+  array (
+    'markuz/almost-lara' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '16da1a62fc2f500c5267b7cedf2c03fbbff1d398',
+    ),
+  ),
+);
