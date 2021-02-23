67890c7e1b14e334d1c20f4d4ba899bdfe33ef02
Saras Maras <sarunas.mailas@gmail.com>
2021-02-23 07:37:13 +0200

    2021.02.23 v3 vlucas added

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit9890acbd0f86605e9fb0ad5dfbae8fee
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit9890acbd0f86605e9fb0ad5dfbae8fee', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit9890acbd0f86605e9fb0ad5dfbae8fee', 'loadClassLoader'));
@@ -50,6 +52,24 @@ class ComposerAutoloaderInit9890acbd0f86605e9fb0ad5dfbae8fee
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit9890acbd0f86605e9fb0ad5dfbae8fee::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire9890acbd0f86605e9fb0ad5dfbae8fee($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire9890acbd0f86605e9fb0ad5dfbae8fee($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,7 +6,7 @@
     'aliases' => 
     array (
     ),
-    'reference' => '16da1a62fc2f500c5267b7cedf2c03fbbff1d398',
+    'reference' => '5d2afd73f3d8e29beba3e7aea7d5da7162ff08c2',
     'name' => 'markuz/almost-lara',
   ),
   'versions' => 
@@ -18,7 +18,34 @@
       'aliases' => 
       array (
       ),
-      'reference' => '16da1a62fc2f500c5267b7cedf2c03fbbff1d398',
+      'reference' => '5d2afd73f3d8e29beba3e7aea7d5da7162ff08c2',
+    ),
+    'phpoption/phpoption' => 
+    array (
+      'pretty_version' => '1.7.5',
+      'version' => '1.7.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '994ecccd8f3283ecf5ac33254543eb0ac946d525',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.19.0',
+      'version' => '1.19.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aed596913b70fae57be53d86faa2e9ef85a2297b',
+    ),
+    'vlucas/phpdotenv' => 
+    array (
+      'pretty_version' => 'v4.2.0',
+      'version' => '4.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'da64796370fc4eb03cc277088f6fede9fde88482',
     ),
   ),
 );
